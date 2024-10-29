01b249ada7684c53f65c083196cf9ff62bd02c88
lpaquatt <lpaquatt@Inception.lpaquatt.42.fr>
2024-10-29 19:44:29 +0100

    ftp pk mais chelou

--- srcs/requirements/bonus/ftp/entrypoint.sh
+++ srcs/requirements/bonus/ftp/entrypoint.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+service vsftpd start
+service vsftpd stop
 
-# Configuration pour activer l'accès anonyme
-sed -i 's/^anonymous_enable=NO/anonymous_enable=YES/' /etc/vsftpd.conf
-
-# Démarre vsftpd en mode non-démon et vérifie le service
-vsftpd /etc/vsftpd.conf &
-
-# Boucle pour vérifier la disponibilité de FTP toutes les secondes
-until netstat -tuln | grep :21; do
-    echo "Attente du démarrage de vsftpd..."
-    sleep 1
-done
-
-# Bloque le script pour empêcher le conteneur de s’arrêter
-tail -f /dev/null
+/usr/sbin/vsftpd
