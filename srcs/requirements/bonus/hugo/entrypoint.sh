2a7c304415da5086a213e7a326a43333799827a2
lpaquatt <lpaquatt@Inception.lpaquatt.42.fr>
2024-10-30 18:10:15 +0100

    static website marche pas

---
+++ srcs/requirements/bonus/hugo/entrypoint.sh
@@ -0,0 +1,9 @@
+hugo new content content/posts/my-first-post.md
+echo "+++
+title = 'My First Post'
+date = 2024-01-14T07:07:07+01:00
+draft = true
++++" > content
+hugo server -D --bind 0.0.0.0
+
+https://www.digitalocean.com/community/tutorials/how-to-install-go-on-debian-10
